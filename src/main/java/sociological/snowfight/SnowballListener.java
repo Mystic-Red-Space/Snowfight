2c2945782a4fbfe852d8419adda206d669e4faed
Socio-path <doohee006@gmail.com>
2019-06-07 19:38:31 +0900

    Create cooldown to snowball

---
+++ src/main/java/sociological/snowfight/SnowballListener.java
@@ -0,0 +1,25 @@
+package sociological.snowfight;
+
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.entity.ProjectileLaunchEvent;
+import org.bukkit.entity.Snowball;
+import org.bukkit.Bukkit;
+import org.bukkit.inventory.ItemStack;
+
+
+public class SnowballListener implements Listener {
+    @EventHandler
+    public void onSnowballThrowed(ProjectileLaunchEvent event) {
+        if(event.getEntity() instanceof Snowball) {
+            Player shooter = (Player)event.getEntity().getShooter();
+            if(shooter.getInventory().contains(Material.SNOW_BLOCK)) {
+                shooter.getInventory().removeItem(new ItemStack(Material.SNOW_BLOCK,1));
+            } else {
+                shooter.setCooldown(Material.SNOWBALL,60);
+            }
+        }
+    }
+}
