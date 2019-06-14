4feb7a4fab2adfb624f9931e74fe4a7630a52c46
Socio_path <doohee006@gmail.com>
2019-06-14 19:39:29 +0900

    Edit what

--- src/main/java/sociological/snowfight/FireballListener.java
+++ src/main/java/sociological/snowfight/FireballListener.java
@@ -3,6 +3,7 @@ package sociological.snowfight;
 import org.bukkit.Material;
 import org.bukkit.entity.Fireball;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.SmallFireball;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEvent;
@@ -12,7 +13,7 @@ public class FireballListener implements Listener {
     public void onFireballClicked(PlayerInteractEvent event) {
         Player player = event.getPlayer();
         if (event.getItem() != null && event.getItem().getType() == Material.FIRE_CHARGE) {
-            Fireball fireball = player.getWorld().spawn(player.getLocation(), Fireball.class);
+            SmallFireball fireball = player.getWorld().spawn(player.getLocation(), SmallFireball.class);
             fireball.setShooter(player);
         }
     }
