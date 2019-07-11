f528f4bf12e92c2234f55f1282ddac172d143ea3
Socio_path <doohee006@gmail.com>
2019-07-11 21:38:45 +0900

    Made ice spear

--- src/main/java/sociological/snowfight/DamageListener.java
+++ src/main/java/sociological/snowfight/DamageListener.java
@@ -1,10 +1,14 @@
 package sociological.snowfight;
 
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Snowball;
+import org.bukkit.entity.Trident;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
 public class DamageListener implements Listener {
     @EventHandler
@@ -16,6 +20,11 @@ public class DamageListener implements Listener {
                 } else {
                     event.setCancelled(true);
                 }
+            } else if (event.getDamager() instanceof Trident) {
+                event.setDamage(2);
+                ((LivingEntity) event.getEntity()).addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS, 60, 255, false, false, false),true);
+                ((LivingEntity) event.getEntity()).addPotionEffect(new PotionEffect(PotionEffectType.JUMP, 60, 128, false, false, false),true);
+                ((LivingEntity) event.getEntity()).addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 60, 255, false, false, false),true);
             }
         }
     }
