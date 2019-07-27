65067848f46806068d1a2afa0b6073e3caa5740c
Socio-path <doohee006@gmail.com>
2019-07-27 16:47:20 +0900

    Change teleport to vector

--- src/main/java/sociological/snowfight/ProjectileHitListener.java
+++ src/main/java/sociological/snowfight/ProjectileHitListener.java
@@ -2,10 +2,12 @@ package sociological.snowfight;
 
 import org.bukkit.Location;
 import org.bukkit.entity.Entity;
+import org.bukkit.entity.Projectile;
 import org.bukkit.entity.Snowball;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.ProjectileHitEvent;
+import org.bukkit.util.Vector;
 
 import java.util.Random;
 
@@ -14,23 +16,25 @@ public class ProjectileHitListener implements Listener {
 
     @EventHandler
     public void OnProjectileHited(ProjectileHitEvent event) {
-        if (event.getEntity() instanceof Snowball && event.getEntity().getCustomName() != null) {
-            if (event.getEntity().getCustomName().equals("전도성 눈덩이")) {
-                Location loc = event.getEntity().getLocation();
-                event.getEntity().getWorld().strikeLightning(loc);
-            } else if (event.getEntity().getCustomName().equals("강제점프 눈덩이") && event.getHitEntity() != null) {
-                event.getHitEntity().teleport(event.getEntity().getLocation().add(0, 4, 0));
-            } else if (event.getEntity().getCustomName().equals("랜덤이동 눈덩이") && event.getHitEntity() != null) {
-                event.getHitEntity().teleport(event.getEntity().getLocation().add(rand.nextInt(11) - 5, 0, rand.nextInt(11) - 5));
-            } else if (event.getEntity().getCustomName().equals("위치교환 눈덩이"))
-                if (event.getHitEntity() != null && event.getEntity().getShooter() != null) {
-                    Location shooterloc = ((Entity) event.getEntity().getShooter()).getLocation();
+        Projectile entity = event.getEntity();
+        if (entity instanceof Snowball && entity.getCustomName() != null) {
+            if (entity.getCustomName().equals("전도성 눈덩이")) {
+                Location loc = entity.getLocation();
+                entity.getWorld().strikeLightning(loc);
+            } else if (entity.getCustomName().equals("강제점프 눈덩이") && event.getHitEntity() != null) {
+                event.getHitEntity().setVelocity(new Vector(0, 4, 0));
+                /*event.getHitEntity().teleport(entity.getLocation().add(0, 4, 0));*/
+            } else if (entity.getCustomName().equals("랜덤이동 눈덩이") && event.getHitEntity() != null) {
+                event.getHitEntity().teleport(entity.getLocation().add(rand.nextInt(11) - 5, 0, rand.nextInt(11) - 5));
+            } else if (entity.getCustomName().equals("위치교환 눈덩이"))
+                if (event.getHitEntity() != null && entity.getShooter() != null) {
+                    Location shooterloc = ((Entity) entity.getShooter()).getLocation();
                     Location hitterloc = event.getHitEntity().getLocation();
-                    ((Entity) event.getEntity().getShooter()).teleport(hitterloc);
-                    event.getHitEntity().teleport(shooterloc);
-                } else if (event.getHitBlock() != null && event.getEntity().getShooter() != null) {
+                    ((Entity) entity.getShooter()).teleport(new Location(entity.getWorld(), hitterloc.getX(), hitterloc.getY(), hitterloc.getZ(), shooterloc.getYaw(), shooterloc.getPitch()));
+                    event.getHitEntity().teleport(new Location(entity.getWorld(), shooterloc.getX(), shooterloc.getY(), shooterloc.getZ(), hitterloc.getYaw(), hitterloc.getPitch()));
+                } else if (event.getHitBlock() != null && entity.getShooter() != null) {
                     Location loc = event.getHitBlock().getLocation();
-                    ((Entity) event.getEntity().getShooter()).teleport(new Location(((Entity) event.getEntity().getShooter()).getWorld(), loc.getX(), loc.getY() + 1, loc.getZ(), ((Entity) event.getEntity().getShooter()).getLocation().getYaw(), ((Entity) event.getEntity().getShooter()).getLocation().getPitch()));
+                    ((Entity) entity.getShooter()).teleport(new Location(((Entity) entity.getShooter()).getWorld(), loc.getX(), loc.getY() + 1, loc.getZ(), ((Entity) entity.getShooter()).getLocation().getYaw(), ((Entity) entity.getShooter()).getLocation().getPitch()));
                 }
         }
     }
